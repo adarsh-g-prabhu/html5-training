a657123bf41cc25500eba1bb48323af5c9ec1b94
Adarsh Prabhu <adarsh.prabhu@aspiresys.kl>
2025-01-02 18:18:16 +0530

    Day 6 Progress

---
+++ JS/objects2.js
@@ -0,0 +1,35 @@
+function person(name,age)
+{
+    this.name=name;
+    this.age=age;
+    this.greetings=function(){
+        console.log("my name is "+this.name+" and i am "+this.age+"years old");
+    };
+}
+
+const persons= new person("rahul",19);
+persons.greetings();
+
+const newPersons= persons;
+newPersons.age=30;
+persons.greetings();
+person.prototype.age=22;
+
+const persons2= new person("babu");
+
+persons2.greetings();
+
+persons.interest="sports";
+console.log(persons.interest);
+
+let text ="";
+for (let [person, value] of Object.entries(persons)) {
+    text += person + ": " + value ;
+  }
+  console.log(text)
+  
+
+delete persons.interest;
+console.log(persons.interest);
+
+
